1d04adf0fccac3adf3a93a04c14f81742630f297
Kranthi <kranthi2595@gmail.com>
2022-12-27 15:09:21 +0530

    Adding Branch Push is successfully completed

--- src/test/java/resturent/EDUBankings.java
+++ src/test/java/resturent/EDUBankings.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class EDUBanking {
+public class EDUBankings {
 	
 	@BeforeTest
 	public void setup() {
