75d424b76389dad81177e6474220b825619260c7
Kranthi <kranthi2595@gmail.com>
2022-12-27 15:05:43 +0530

    Adding different Branch is successfull.

---
+++ src/test/java/resturent/EDUBanking.java
@@ -0,0 +1,25 @@
+package resturent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EDUBanking {
+	
+	@BeforeTest
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/Lenovo/Desktop/Automation%20Testing/PSRestuarant/index.html");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void customerlogin() {
+		
+	}
+
+}
